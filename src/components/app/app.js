6f67e5548f856f32a875da40a6c2ac0ad01edbf0
gleb <gyrra98@mail.ru>
2021-07-23 20:30:10 +0300

    applied React property, state of components and events listeners

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,14 @@ import PostAddForm from '../post-add-form'
 import './app.css'
 
 const App = () => {
+
+    const data = [
+        {label: "Going to learn React", important: true, id: "wergawerghil"},
+        {label: "That is so good", important: false, id: "weufhiulwehfil"},
+        {label: "I need a break .....", important: false, id: "fuiwefhui"}
+    ]
+
+
     return (
         <div className="app">
             <AppHeader/>
@@ -14,7 +22,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data} />
             <PostAddForm/>
         </div>
     )
